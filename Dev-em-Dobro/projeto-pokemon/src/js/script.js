a011ad6946740489e7a4e6351a97f36d2d4c6c80
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-08 09:00:34 -0300

    Adiciona JS para mudanção de tema (ligth e dark) Projeto POKEMON

--- Dev-em-Dobro/projeto-pokemon/src/js/script.js
+++ Dev-em-Dobro/projeto-pokemon/src/js/script.js
@@ -1,1 +1,22 @@
-console.log ("HelloWorld");
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocDeTema = document.getElementById("imagem-botao");
+
+//Função de seta 
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+
+
+    if (modoEscuroAtivo) {
+        body.classList.remove("modo-escuro")
+        imagemBotaoTrocDeTema.setAttribute("src", "src/image/sun.png")
+
+    } else {
+
+        body.classList.add("modo-escuro")
+        imagemBotaoTrocDeTema.setAttribute("src", "src/image/moon.png")
+    }
+
+
+})
