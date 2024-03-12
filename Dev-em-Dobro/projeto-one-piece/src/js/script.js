03ad7698c40b2ee0f95fb82c078de38e4c974b08
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-12 07:44:18 -0300

    Adicionado JS no Projeto ONE PIECE

---
+++ Dev-em-Dobro/projeto-one-piece/src/js/script.js
@@ -0,0 +1,18 @@
+const botoesPersonagem = document.querySelectorAll('.personagem-btn button');
+    const imagensPersonagem = document.querySelectorAll('.personagens .personagem');
+
+    botoesPersonagem.forEach((botao, index) => {
+        botao.addEventListener('click', function() {
+            botoesPersonagem.forEach(botao => {
+                botao.parentElement.classList.remove('selecionado');
+            });
+
+            this.parentElement.classList.add('selecionado');
+
+            imagensPersonagem.forEach(imagem => {
+                imagem.style.display = 'none';
+            });
+
+            imagensPersonagem[index].style.display = 'block';
+        });
+    });
