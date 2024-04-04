f3ec994d6695c8d64e51acf08cccd8d085fe6dc9
Gabrielygor <gabrielcanuto042@gmail.com>
2024-04-04 09:57:00 -0300

    Removendo Projeto Forms

--- Rocketseat/Forms/src/js/theme-dark-ligth.js
+++
@@ -1,8 +0,0 @@
-const btnTema = document.querySelector(".botao-tema")
-const body = document.querySelector("body")
-
-
-btnTema.addEventListener("click", () => {
-    body.classList.toggle("ligth")
-    console.log(body);
-});
