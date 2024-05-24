af4665351198a1892a1721ce21d67220fbc9a1ca
Gabrielygor <gabrielcanuto042@gmail.com>
2024-05-24 13:19:31 -0300

    Add variaveis no css

--- Testes/darkTheme/index.js
+++ Testes/darkTheme/index.js
@@ -2,7 +2,7 @@ const themeBtn = document.getElementById('theme-btn');
 const body = document.getElementById('body');
 
 themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+    document.body.classList.toggle('dark-theme');
     console.log(body.classList);
 });
 
