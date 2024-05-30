4c41fc0281b42c89d8693026758e7ef421baa247
Gabrielygor <gabrielcanuto042@gmail.com>
2024-05-30 14:39:07 -0300

    Add presatmospheric thingspeak

--- Testes/dashboard/src/js/thingspeakapi.js
+++ Testes/dashboard/src/js/thingspeakapi.js
@@ -1,7 +1,8 @@
 async function fetchData() {
     const urlTemperature = 'https://api.thingspeak.com/channels/1293177/fields/1/last.json';
     const urlHumidity = 'https://api.thingspeak.com/channels/1293177/fields/2/last.json';
-    
+    const urlPresAtmospheric = 'https://api.thingspeak.com/channels/1293177/fields/3/last.json';
+
     try {
         const responseTemperature = await fetch(urlTemperature);
         const dataTemperature = await responseTemperature.json();
@@ -10,14 +11,20 @@ async function fetchData() {
         const responseHumidity = await fetch(urlHumidity);
         const dataHumidity = await responseHumidity.json();
         const humidity = dataHumidity.field2;
+
+        const responsePresAtmospheric = await fetch(urlPresAtmospheric);
+        const dataPresAtmospheric = await responsePresAtmospheric.json();
+        const presAtmospheric = dataPresAtmospheric.field3;
         
         document.getElementById('temperature-value').textContent = `${temperature}°C`;
         document.getElementById('humidity-value').textContent = `${humidity}%`;
+        document.getElementById('pres-atmospheric-value').textContent = `${presAtmospheric}`
     } catch (error) {
         console.error('Erro ao buscar dados da API ThingSpeak:', error);
         document.getElementById('temperature-value').textContent = 'Erro ao carregar temperatura';
         document.getElementById('humidity-value').textContent = 'Erro ao carregar umidade';
+        document.getElementById('pres-atmospheric-value').textContent = 'Erro ao carregar umidade';
     }
 }
 
-window.onload = fetchData;
+fetchData();
