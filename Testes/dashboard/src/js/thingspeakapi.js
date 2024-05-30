dbccda040aaa2a1c196f68dec5d47231a959f384
Gabrielygor <gabrielcanuto042@gmail.com>
2024-05-30 14:03:38 -0300

    Add Humidade thingSpeak api

--- Testes/dashboard/src/js/thingspeakapi.js
+++ Testes/dashboard/src/js/thingspeakapi.js
@@ -1,16 +1,23 @@
-
-
-async function fetchTemperature() {
-    const url = 'https://api.thingspeak.com/channels/1293177/fields/1/last.json';
+async function fetchData() {
+    const urlTemperature = 'https://api.thingspeak.com/channels/1293177/fields/1/last.json';
+    const urlHumidity = 'https://api.thingspeak.com/channels/1293177/fields/2/last.json';
+    
     try {
-        const response = await fetch(url);
-        const data = await response.json();
-        const temperature = data.field1;
+        const responseTemperature = await fetch(urlTemperature);
+        const dataTemperature = await responseTemperature.json();
+        const temperature = dataTemperature.field1;
+        
+        const responseHumidity = await fetch(urlHumidity);
+        const dataHumidity = await responseHumidity.json();
+        const humidity = dataHumidity.field2;
         
         document.getElementById('temperature-value').textContent = `${temperature}°C`;
+        document.getElementById('humidity-value').textContent = `${humidity}%`;
     } catch (error) {
         console.error('Erro ao buscar dados da API ThingSpeak:', error);
         document.getElementById('temperature-value').textContent = 'Erro ao carregar temperatura';
+        document.getElementById('humidity-value').textContent = 'Erro ao carregar umidade';
     }
 }
-window.onload = fetchTemperature;
+
+window.onload = fetchData;
